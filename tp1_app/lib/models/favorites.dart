4671a62d004253c5bdf4d8daee32fe95c1fc6ada
donia <donia.sakji@gmail.com>
2024-02-11 12:14:43 +0100

    mise a jour de favoriteModel

--- tp1_app/lib/models/favorites.dart
+++ tp1_app/lib/models/favorites.dart
@@ -4,15 +4,15 @@ import 'package:TP1_app/models/book.dart';
 class Favorites extends ChangeNotifier {
   final List<Book> _favoriteBooks = [];
 
-  List<Book> get books => _favoriteBooks;
+  List<Book> get favoritesBooks => _favoriteBooks;
 
-  void add(int itemNo) {
-    _favoriteBooks.add(itemNo);
+  void add(Book bookToAdd) {
+    _favoriteBooks.add(bookToAdd);
     notifyListeners();
   }
 
-  void remove(int itemNo) {
-    _favoriteBooks.remove(itemNo);
+  void remove(Book bookToRemove) {
+    _favoriteBooks.remove(bookToRemove);
     notifyListeners();
   }
 }
