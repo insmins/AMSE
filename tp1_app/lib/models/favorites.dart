e694a21cf441f82e1ae8c7e8f330a92afc5ccfa1
donia <donia.sakji@gmail.com>
2024-02-11 12:24:56 +0100

    rajout du nom items

--- tp1_app/lib/models/favorites.dart
+++ tp1_app/lib/models/favorites.dart
@@ -4,7 +4,7 @@ import 'package:TP1_app/models/book.dart';
 class Favorites extends ChangeNotifier {
   final List<Book> _favoriteBooks = [];
 
-  List<Book> get favoritesBooks => _favoriteBooks;
+  List<Book> get items => _favoriteBooks;
 
   void add(Book bookToAdd) {
     _favoriteBooks.add(bookToAdd);
