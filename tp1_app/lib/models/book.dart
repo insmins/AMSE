6f0a9125e212dcea837b985555f3889c4e4abe44
donia <donia.sakji@gmail.com>
2024-02-12 15:11:54 +0100

    code cleaned

--- tp1_app/lib/models/book.dart
+++ tp1_app/lib/models/book.dart
@@ -3,21 +3,16 @@ class Book {
   String _title;
   String _author;
   String _category;
-  bool _isFavorite;
   String _desc;
 
-  Book(this._title, this._author, this._category, this._isFavorite, this._desc, [this._imgUrl]);
+  Book(this._title, this._author, this._category, this._desc, [this._imgUrl]);
 
  /*------------------ Getters ------------------*/
   String? get imgUrl => _imgUrl;
   String get title => _title;
   String get author => _author;
   String get category => _category;
-  bool get isFavorite => _isFavorite;
   String get desc => _desc;
-/*---------------- isFavorite Setter-------------*/
-  set isFavorite(bool value) {
-    _isFavorite = value;
-  }
+
 
 }
