814a255cfb0d57959bf6d58e65a6e36973b86ce8
Inès El Hadri <ines.el.hadri@etu.imt-nord-europe.fr>
2024-02-18 03:21:08 +0100

    end exo 2

--- tp2_app/lib/screens/exo2.dart
+++ tp2_app/lib/screens/exo2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Exo2 extends StatefulWidget {
   const Exo2({super.key});
@@ -9,7 +10,9 @@ class Exo2 extends StatefulWidget {
 }
 
 class _Exo2State extends State<Exo2> {
-  double _currentSliderValue = 20;
+  double _sliderSki = 0.0;
+  double _sliderHorloge = 0.0;
+  double _sliderDepl = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +20,60 @@ class _Exo2State extends State<Exo2> {
       appBar: AppBar(title: const Text('Transformation d\'images')),
       body: Column(
         children:[
-          Image(image: NetworkImage("https://picsum.photos/512/1024")),
-          Slider(
-            value: _currentSliderValue,
-            max: 100,
-            divisions: 5,
-            label: _currentSliderValue.round().toString(),
-            onChanged: (double value) {
-              setState(() {
-                _currentSliderValue = value;
-              });
-            },
+          Transform(
+            transform: Matrix4.skewY(_sliderSki)..rotateZ(_sliderHorloge)..leftTranslate(_sliderDepl),
+            child: Image(
+                image: ResizeImage(
+                    NetworkImage("https://picsum.photos/512/1024"),
+                    height: 192,
+                    width: 108,
+                )
+            )
+          ),
+          Row(children: [
+            Slider(
+              value: _sliderSki,
+              max: 1,
+              divisions: 10,
+              label: _sliderSki.toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _sliderSki = value;
+                });
+              },
+            ),
+            Text("Cisaillement"),
+          ]
+          ),
+          Row(children: [
+            Slider(
+              value: _sliderHorloge,
+              max: 6.28,
+              divisions: 16,
+              label: _sliderHorloge.toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _sliderHorloge = value;
+                });
+              },
+            ),
+            Text("Rotation"),
+          ]
+          ),
+          Row(children: [
+            Slider(
+              value: _sliderDepl,
+              max: 30,
+              divisions: 15,
+              label: _sliderDepl.toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _sliderDepl = value;
+                });
+              },
+            ),
+            Text("Translation"),
+          ]
           ),
         ]
       ),
