8291a9f2d18074be25d4bf60c74bf22c92255232
Inès El Hadri <ines.el.hadri@etu.imt-nord-europe.fr>
2024-02-28 22:22:17 +0100

    better button show image

--- tp2_app/lib/screens/exo7b.dart
+++ tp2_app/lib/screens/exo7b.dart
@@ -7,8 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:tp2_app/screens/exo1.dart';
 import 'package:tp2_app/screens/exo4.dart';
 
-
-
 // =================
 // Model
 //==================
@@ -20,7 +18,11 @@ class WidgetTile {
   late bool isTileVoid;
   late int order;
 
-  WidgetTile({required this.tile, required this.isClickable, required this.isTileVoid, required this.order});
+  WidgetTile(
+      {required this.tile,
+      required this.isClickable,
+      required this.isTileVoid,
+      required this.order});
 }
 // ==============
 // Widgets
@@ -30,7 +32,12 @@ class Exo7b extends StatefulWidget {
   final List<Widget> tiles;
   final double numberOfRows;
   final double levelOfShuffle;
-  const Exo7b({super.key,required this.tiles, required this.numberOfRows, required this.levelOfShuffle});
+
+  const Exo7b(
+      {super.key,
+      required this.tiles,
+      required this.numberOfRows,
+      required this.levelOfShuffle});
 
   @override
   State<StatefulWidget> createState() => PositionedTilesState();
@@ -38,9 +45,10 @@ class Exo7b extends StatefulWidget {
 
 class PositionedTilesState extends State<Exo7b> {
   late int indexVoidTile; // index tile blanche
-  late  List <WidgetTile> boardTile; // plateau
+  late List<WidgetTile> boardTile; // plateau
+  late List<WidgetTile> rightOrderBoardTile;
   int nbCout = 0;
-  bool isWin =false;
+  bool isWin = false;
   final winController = ConfettiController();
 
   @override
@@ -48,6 +56,7 @@ class PositionedTilesState extends State<Exo7b> {
     super.initState();
     //1. on créé la list de tile qui sont ici des widgets, c'est le plateau
     boardTile = buildWidgetTile();
+    rightOrderBoardTile = buildWidgetTile();
     //2. on identifie l'index de la Tile est "vide"
     indexVoidTile = findVoidTileIndex();
     //3. On mélange les tiles, de sorte que le puzzle soit résolvable
@@ -59,19 +68,28 @@ class PositionedTilesState extends State<Exo7b> {
   }
 
   //1. creation du plateau
-  List<WidgetTile> buildWidgetTile () {
+  List<WidgetTile> buildWidgetTile() {
     List<WidgetTile> widgetTiles = List<WidgetTile>.generate(
       widget.tiles.length,
-          (index) {
-        if (index < (widget.tiles.length -1)) {
-          return (WidgetTile(tile : widget.tiles[index],isClickable: false, isTileVoid: false, order: index));
+      (index) {
+        if (index < (widget.tiles.length - 1)) {
+          return (WidgetTile(
+              tile: widget.tiles[index],
+              isClickable: false,
+              isTileVoid: false,
+              order: index));
         } else {
-          return (WidgetTile(tile: widget.tiles[index], isClickable: false, isTileVoid: true, order: index)); // Blanc
+          return (WidgetTile(
+              tile: widget.tiles[index],
+              isClickable: false,
+              isTileVoid: true,
+              order: index)); // Blanc
         }
       },
     );
     return widgetTiles;
   }
+
 // 2.
   int findVoidTileIndex() {
     for (int index = 0; index < boardTile.length; index++) {
@@ -81,16 +99,19 @@ class PositionedTilesState extends State<Exo7b> {
     }
     return -1; //  -1 si aucune tuile avec isTileVoid == true
   }
+
   //3.
   void shuffleTiles() {
     Random random = Random();
-    double level = (widget.numberOfRows*widget.numberOfRows*widget.levelOfShuffle)*widget.levelOfShuffle;
+    double level =
+        (widget.numberOfRows * widget.numberOfRows * widget.levelOfShuffle) *
+            widget.levelOfShuffle;
     int previousTileIndex = 999;
-    for (int i = 0; i < level.toInt() ; i++) {
-      List <int> adjacentIndices = findAdjacentTiles(indexVoidTile);
+    for (int i = 0; i < level.toInt(); i++) {
+      List<int> adjacentIndices = findAdjacentTiles(indexVoidTile);
       int randomIndex;
       do {
-         randomIndex = adjacentIndices[random.nextInt(adjacentIndices.length)];
+        randomIndex = adjacentIndices[random.nextInt(adjacentIndices.length)];
       } while (randomIndex == previousTileIndex);
 
       swipeTiles(randomIndex);
@@ -100,64 +121,68 @@ class PositionedTilesState extends State<Exo7b> {
   }
 
 //4
-  void setClick (List<int> adjacentIndices){
+  void setClick(List<int> adjacentIndices) {
     // on met toutes les tiles à isClickable false
-    boardTile.forEach  ((tile) => tile.isClickable = false);
+    boardTile.forEach((tile) => tile.isClickable = false);
     // on met les tuiles adjacente clickable
     for (int adjacentIndex in adjacentIndices) {
-      boardTile[adjacentIndex].isClickable  = true;
+      boardTile[adjacentIndex].isClickable = true;
     }
   }
 
-  List<int>  findAdjacentTiles(int indexVoidTile) {
-
+  List<int> findAdjacentTiles(int indexVoidTile) {
     int gridSize = widget.numberOfRows.toInt();
     int row = indexVoidTile ~/ gridSize; // la ligne où est la tile blanche
     int col = indexVoidTile % gridSize; // colone où est la tile blanche
 // on chercher les tuiles adjacentes
     List<int> adjacentIndices = [];
 
-    if (row > 0) adjacentIndices.add(indexVoidTile - gridSize); // Tuile au-dessus
-    if (row < gridSize - 1) adjacentIndices.add(indexVoidTile + gridSize); // Tuile en dessous
+    if (row > 0)
+      adjacentIndices.add(indexVoidTile - gridSize); // Tuile au-dessus
+    if (row < gridSize - 1)
+      adjacentIndices.add(indexVoidTile + gridSize); // Tuile en dessous
     if (col > 0) adjacentIndices.add(indexVoidTile - 1); // Tuile à gauche
-    if (col < gridSize - 1) adjacentIndices.add(indexVoidTile + 1); // Tuile à droite
-    return  adjacentIndices;
-
+    if (col < gridSize - 1)
+      adjacentIndices.add(indexVoidTile + 1); // Tuile à droite
+    return adjacentIndices;
   }
 
   void onAdjacentTileTap(int adjacentIndex) {
     setState(() {
       swipeTiles(adjacentIndex);
       indexVoidTile = adjacentIndex; // on change la tile blanche
-      setClick(findAdjacentTiles(indexVoidTile));// on met a jour les tuiles clickable
+      setClick(findAdjacentTiles(
+          indexVoidTile)); // on met a jour les tuiles clickable
       nbCout++;
     });
     checkWin();
   }
 
-  void swipeTiles (int index){
+  void swipeTiles(int index) {
     WidgetTile temp = boardTile[indexVoidTile];
     boardTile[indexVoidTile] = boardTile[index];
     boardTile[index] = temp;
   }
 
-void checkWin() {
-  for (int i = 0; i < boardTile.length; i++) {
-    if (boardTile[i].order != i) { // si on a pas le bon ordre on sort
-      return;
+  void checkWin() {
+    for (int i = 0; i < boardTile.length; i++) {
+      if (boardTile[i].order != i) {
+        // si on a pas le bon ordre on sort
+        return;
+      }
     }
+    print("Win !");
+    setState(() {
+      winController.play();
+      isWin = true;
+    });
   }
-  print("Win !");
-  setState(() {
-    winController.play();
-    isWin = true;
-  });
-}
 
   int heures = 0;
   int minutes = 0;
   int secondes = 0;
-  void stopWatch(Timer t){
+
+  void stopWatch(Timer t) {
     setState(() {
       secondes = secondes + 1;
       if (secondes == 60) {
@@ -171,7 +196,7 @@ void checkWin() {
     });
   }
 
-
+  bool showImage = false;
 
   @override
   Widget build(BuildContext context) {
@@ -195,24 +220,32 @@ void checkWin() {
                   crossAxisCount: widget.numberOfRows.toInt(),
                   children: [
                     for (int index = 0; index < boardTile.length; index++)
-                      InkWell(
-                        onTap: boardTile[index].isClickable
-                            ? () {
-                          onAdjacentTileTap(index);
-                        }
-                            : null,
-                        child: Visibility(
-                          visible: !boardTile[index].isTileVoid,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: boardTile[index].isClickable
-                                  ? Border.all(color: Colors.red, width: 3)
-                                  : null,
+                      if (showImage)
+                        Container(
+                          child: rightOrderBoardTile[index].tile,
+                        )
+                      else
+                        Container(
+                          decoration: BoxDecoration(
+                            border: boardTile[index].isClickable
+                                ? Border.all(color: Colors.red, width: 3)
+                                : null,
+                          ),
+                          child: InkWell(
+                            onTap: boardTile[index].isClickable
+                                ? () {
+                                    onAdjacentTileTap(index);
+                                  }
+                                : null,
+                            child: Visibility(
+                              visible: !boardTile[index].isTileVoid,
+                              child: Opacity(
+                                opacity: boardTile[index].isClickable ? 0.4 : 1,
+                                child: boardTile[index].tile,
+                              ),
                             ),
-                            child: boardTile[index].tile,
                           ),
                         ),
-                      ),
                   ],
                 ),
               ),
@@ -223,25 +256,35 @@ void checkWin() {
               emissionFrequency: 0.1,
             ),
             Padding(padding: EdgeInsets.all(30)),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(nbCout.toString(),
-                      style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.5, color: Colors.deepPurple, decoration: TextDecoration.none)
-                  ),
-                  Text(" coups.",
-                  style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.01, color: Colors.black, decoration: TextDecoration.none )),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(nbCout.toString(),
+                  style: DefaultTextStyle.of(context).style.apply(
+                      fontSizeFactor: 1.5,
+                      color: Colors.deepPurple,
+                      decoration: TextDecoration.none)),
+              Text(" coups.",
+                  style: DefaultTextStyle.of(context).style.apply(
+                      fontSizeFactor: 1.01,
+                      color: Colors.black,
+                      decoration: TextDecoration.none)),
             ]),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Temps écoulé : " + heures.toString().padLeft(2, '0') + ":"
-                      + minutes.toString().padLeft(2, '0') + ":"
-                      + secondes.toString().padLeft(2, '0'),
-                    style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 0.5, color: Colors.black, decorationColor: Colors.deepPurple),
-                  ),
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Temps écoulé : " +
+                      heures.toString().padLeft(2, '0') +
+                      ":" +
+                      minutes.toString().padLeft(2, '0') +
+                      ":" +
+                      secondes.toString().padLeft(2, '0'),
+                  style: DefaultTextStyle.of(context).style.apply(
+                      fontSizeFactor: 0.5,
+                      color: Colors.black,
+                      decorationColor: Colors.deepPurple),
+                ),
+              ],
+            ),
             Visibility(
               visible: isWin,
               child: Column(
@@ -256,9 +299,14 @@ void checkWin() {
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => Exo7b(tiles: widget.tiles,numberOfRows: widget.numberOfRows,levelOfShuffle: widget.levelOfShuffle),
-                      ));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Exo7b(
+                                tiles: widget.tiles,
+                                numberOfRows: widget.numberOfRows,
+                                levelOfShuffle: widget.levelOfShuffle),
+                          ));
                     },
                     child: const Text('Nouvelle partie'),
                   ),
@@ -271,12 +319,11 @@ void checkWin() {
               child: IconButton(
                 color: Colors.white,
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (context) => Exo1(),
-                  ));
+                  setState(() {
+                    showImage = !showImage;
+                  });
                 },
                 icon: const Icon(Icons.image),
-
               ),
             ),
           ],
@@ -284,6 +331,4 @@ void checkWin() {
       ),
     );
   }
-
-
 }
